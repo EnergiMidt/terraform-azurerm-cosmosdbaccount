3fffd6edbb8834da8dc12b0878f93eed19bb4322
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@computas.com>
2022-10-03 13:20:22 +0200

    docs: update description

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "azurerm_cosmosdb_account" {
-  value = local.cosmosdb_account
+  description = "The Azure CosmosDB Account resource."
+  value       = local.cosmosdb_account
 }
 
 output "azurerm_cosmosdb_account_id" {
-  value = local.cosmosdb_account.id
+  description = "The ID of Azure CosmosDB Account resource."
+  value       = local.cosmosdb_account.id
 }
